f2998ed55d7f51ae2da3c27da8d2668a5fc32b60
rubenh05-prog <rubenh05@googlemail.com>
2026-01-03 19:49:51 +0100

    service-worker.js aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "3d-print-costs-v1";
+const CACHE_NAME = "3d-print-costs-v1";
 const FILES = [
   "./",
   "./index.html",
@@ -7,12 +7,12 @@ const FILES = [
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
