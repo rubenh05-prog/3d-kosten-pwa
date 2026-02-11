987bcd34b09355b683edd8b66d814bc426cd5dc1
Robin <rubenh05@googlemail.com>
2026-02-11 16:50:06 +0100

    Update service worker version to v19.2

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
    4) Iteratives Cache-Limit (keine Rekursion)
    ========================================== */
 
-const VERSION = "v19.1";
+const VERSION = "v19.2";
 
 const HTML_CACHE   = `html-${VERSION}`;
 const ASSET_CACHE  = `assets-${VERSION}`;
